2b6f25297c7b018d985e3402e4c2d99d65ae3ff7
hhofent <15273623+hhofent@users.noreply.github.com>
2023-08-20 15:50:34 -0500

    all done. need reset button but too lazy; hit F5

--- javascript.js
+++ javascript.js
@@ -6,7 +6,11 @@ let cScore = 0;
 let pScore = 0;
 let bestOf;
 let chosen = false;
-const resultDisplay = document.querySelector('#resultDisplay');
+let started = false;
+const resultCont = document.querySelector('.resultCont');
+const resultDisplay = document.querySelector('.resultDisplay');
+const resultTop = document.querySelector('.resultTop');
+const resultBottom = document.querySelector('.resultBottom');
 const playerScore = document.querySelector('#playerScore');
 const computerScore = document.querySelector('#computerScore');
 const playerChoiceCont = document.querySelector("#playerChoiceCont");
@@ -25,24 +29,21 @@ const rockButton = document.querySelector("#rockButton");
 rockButton.addEventListener("click", () => {
     playerChoice = 'rock';
     chosen = true;
-    bestOf = roundSelect.value;
-    game(bestOf);
+    startGame();
 });
 
 const paperButton = document.querySelector('#paperButton');
 paperButton.addEventListener("click", () => {
     playerChoice = 'paper';
     chosen = true;
-    bestOf = roundSelect.value;
-    game(bestOf);
+    startGame();
 });
 
 const scissorsButton = document.querySelector('#scissorsButton');
 scissorsButton.addEventListener("click", () => {
     playerChoice = 'scissors';
     chosen = true;
-    bestOf = roundSelect.value;
-    game(bestOf);
+    startGame();
 });
 
 const playRound = (playerChoice, computerChoice) => {
@@ -54,38 +55,64 @@ const playRound = (playerChoice, computerChoice) => {
 };
 
 const game = (bestOf) => {
-    if  (chosen == true) {
-        if ((cScore < bestOf) && (pScore < bestOf)) {
-            getComputerChoice();
-            result = playRound(playerChoice, computerChoice);
+    if ((cScore < bestOf) && (pScore < bestOf)) {
+        started = true;
+        getComputerChoice();
+        result = playRound(playerChoice, computerChoice);
 
-            if (result === "You've lost this round.") {
-                ++cScore;
-            } else if (result === "You've won this round.") {
-                ++pScore;
-            };
-            
-            playerScore.textContent = pScore;
-            playerScore.append;
-            playerChoiceCont.textContent = playerChoice;
-            playerChoiceCont.append;
-            computerScore.textContent = cScore;
-            computerScore.append;
-            computerChoiceCont.textContent = computerChoice;
-            computerChoiceCont.append;
-            
-            if (pScore == bestOf) {
-                resultDisplay.textContent = "You've won! You've beaten a simple machine by pure chance "+pScore+"-"+cScore;
-                resultDisplay.append;
-            };
-            if (cScore == bestOf) {
-                resultDisplay.textContent = "You've lost! Imagine losing to a robot "+cScore+"-"+pScore;
-                resultDisplay.append;
-            };
-            
-            chosen = false;
+        if (result === "You've lost this round.") {
+            ++cScore;
+        } else if (result === "You've won this round.") {
+            ++pScore;
         };
-    };
+            
+        playerScore.textContent = pScore;
+        playerScore.append;
+        playerChoiceCont.textContent = playerChoice;
+        playerChoiceCont.append;
+        computerScore.textContent = cScore;
+        computerScore.append;
+        computerChoiceCont.textContent = computerChoice;
+        computerChoiceCont.append;
+            
+        if (pScore == bestOf) {
+            resultDisplay.textContent = "You've won! You've beaten a simple machine by pure chance "+pScore+"-"+cScore;
+            resultDisplay.classList.add('shownDisplay');
+            resultCont.classList.add('shownCont');
+            resultTop.classList.add('shownTop');
+            resultBottom.classList.add('shownBottom');
+            resultDisplay.append;
+            resultCont.append;
+            resultTop.append;
+            resultBottom.append;
+        };
+        if (cScore == bestOf) {
+            resultDisplay.textContent = "You've lost! Imagine losing to a robot "+cScore+"-"+pScore;
+            resultDisplay.classList.add('shownDisplay');
+            resultCont.classList.add('shownCont');
+            resultTop.classList.add('shownTop');
+            resultBottom.classList.add('shownBottom');
+            resultDisplay.append;
+            resultCont.append;
+            resultTop.append;
+            resultBottom.append;
+        };
+            
+        chosen = false;
+    };    
 };
 
+const start = document.querySelector('#start');
+start.addEventListener("click", () => {
+    bestOf = roundSelect.value;
+    console.log(bestOf);
+    document.getElementById('selecterCont').style.opacity = "0";
+    document.getElementById('startCont').style.opacity = "0";
+    startGame();
+})
 
+const startGame = () => {
+    if (chosen == true) {
+        game(bestOf);
+    }
+};
